c071bc81c4897c01849325af44b3a201de9025d1
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2026-09-02 13:22:23 +0530

    java intro

---
+++ Class-1/Test.java
@@ -0,0 +1,11 @@
+public class Test{
+    int tv9=100;
+
+    public boolean login(){
+        return true;
+    }
+    public static void main(String[] args) {
+        int thread=100;
+        System.out.println(thread);
+    }
+}
